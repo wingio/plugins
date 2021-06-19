2b21aa2221a0e14544c774f944e0f61dbadf619e
wingio <mcprowing@gmail.com>
2021-06-19 18:46:12 -0400

    demo

--- Deez/src/main/java/com/aliucord/plugins/deez.java
+++ Deez/src/main/java/com/aliucord/plugins/deez.java
@@ -1,23 +1,24 @@
 package com.aliucord.plugins;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 
-import com.aliucord.Main;
+import com.aliucord.api.CommandsAPI;
 import com.aliucord.entities.Plugin;
-
 import java.util.*;
 
 @SuppressWarnings("unused")
-public class ExamplePlugin extends Plugin {
+public class deez extends Plugin {
+
     @NonNull
     @Override
     public Manifest getManifest() {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[]{ new Manifest.Author("Wing", 298295889720770563L) };
-        manifest.description = "Does a thing";
-        manifest.version = "1.0.0";
+        manifest.description = "Deez";
+        manifest.version = "1.0";
         manifest.updateUrl = "https://raw.githubusercontent.com/wingio/plugins/builds/updater.json";
         return manifest;
     }
@@ -35,10 +36,13 @@ public class ExamplePlugin extends Plugin {
                 Clap = TextUtils.join(" :clap: ", msg.split(" ", 0));
             }
             return new CommandsAPI.CommandResult(Clap);
+        });
+
     }
 
     @Override
     public void stop(Context context) {
-        patcher.unpatchAll();
+        commands.unregisterAll();
     }
+
 }
