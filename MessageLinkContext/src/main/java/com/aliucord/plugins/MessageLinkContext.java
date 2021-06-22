f2fbfd756c125cb080489e23ad8efc1397d296ee
wingio <mcprowing@gmail.com>
2021-06-21 23:08:17 -0400

    official release

--- MessageLinkContext/src/main/java/com/aliucord/plugins/MessageLinkContext.java
+++ MessageLinkContext/src/main/java/com/aliucord/plugins/MessageLinkContext.java
@@ -38,7 +38,7 @@ public class MessageLinkContext extends Plugin {
       };
     manifest.description =
       "Adds a context menu option to copy the message link";
-    manifest.version = "1.0.1";
+    manifest.version = "1.1.0";
     manifest.updateUrl =
       "https://raw.githubusercontent.com/wingio/plugins/builds/updater.json";
     return manifest;
