508cfa1b0b6676328c851576faa4a518191d2d7a
wingio <mcprowing@gmail.com>
2021-06-22 14:24:29 -0400

    move option next to share

--- MessageLinkContext/src/main/java/com/aliucord/plugins/MessageLinkContext.java
+++ MessageLinkContext/src/main/java/com/aliucord/plugins/MessageLinkContext.java
@@ -40,7 +40,7 @@ public class MessageLinkContext extends Plugin {
       };
     manifest.description =
       "Adds a context menu option to copy the message link";
-    manifest.version = "1.3.1";
+    manifest.version = "1.3.2";
     manifest.updateUrl =
       "https://raw.githubusercontent.com/wingio/plugins/builds/updater.json";
     return manifest;
@@ -123,7 +123,7 @@ public class MessageLinkContext extends Plugin {
               Utils.showToast(context, "Copied link");
             }
           );
-          layout.addView(view, 1);
+          layout.addView(view, 6);
         }
       )
     );
