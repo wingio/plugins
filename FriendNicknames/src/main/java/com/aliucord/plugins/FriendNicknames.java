8cb62f73eb7ff3bb16a7d1537fb6cd4d3a42fc94
wingio <mcprowing@gmail.com>
2021-06-27 18:40:27 -0400

    test

--- FriendNicknames/src/main/java/com/aliucord/plugins/FriendNicknames.java
+++ FriendNicknames/src/main/java/com/aliucord/plugins/FriendNicknames.java
@@ -49,7 +49,7 @@ public class FriendNicknames extends Plugin {
   @Override
   @SuppressWarnings({ "unchecked", "ConstantConditions" })
   public void start(Context context) {
-    var subcommands = new List<ApplicationCommandOption>();
+    var subcommands = new Collections.emptyList();
     var userOption = new ApplicationCommandOption(
       ApplicationCommandType.USER,
       "user",
