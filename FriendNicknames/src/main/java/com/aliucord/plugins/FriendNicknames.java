018d1b2188ce8afd02156c1339789b9905838217
wingio <mcprowing@gmail.com>
2021-06-27 19:34:53 -0400

    test

--- FriendNicknames/src/main/java/com/aliucord/plugins/FriendNicknames.java
+++ FriendNicknames/src/main/java/com/aliucord/plugins/FriendNicknames.java
@@ -10,11 +10,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.aliucord.PluginManager;
 import com.aliucord.Utils;
+import com.aliucord.api.CommandsAPI;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.PinePatchFn;
 import com.aliucord.widgets.LinearLayout;
 import com.discord.api.channel.Channel;
-import com.aliucord.api.CommandsAPI;
 import com.discord.api.commands.ApplicationCommandType;
 import com.discord.api.commands.CommandChoice;
 import com.discord.app.AppBottomSheet;
@@ -30,7 +30,7 @@ import java.util.*;
 
 @SuppressWarnings({ "unchecked", "unused" })
 public class FriendNicknames extends Plugin {
-  
+
   @NonNull
   @Override
   public Manifest getManifest() {
@@ -49,6 +49,26 @@ public class FriendNicknames extends Plugin {
   @Override
   @SuppressWarnings({ "unchecked", "ConstantConditions" })
   public void start(Context context) {
+    patcher.patch(
+      GuildMember.Companion.getClass(),
+      "getNickOrUsername",
+      new Class<?>[] {
+        User.class,
+        GuildMember.class,
+        Channel.class,
+        List.class,
+      },
+      new PinePatchFn(
+        callFrame -> {
+          var user = (User) callFrame.args[0];
+          var userId = user.getId();
+          var nickname = sets.getString(String.valueOf(userId), null);
+          Utils.log(nickname);
+          if (nickname == null) return
+          callFrame.setResult(nickname);
+        }
+      )
+    );
     var userOption = new ApplicationCommandOption(
       ApplicationCommandType.USER,
       "user",
@@ -71,67 +91,53 @@ public class FriendNicknames extends Plugin {
     );
 
     var setOption = new ApplicationCommandOption(
-        ApplicationCommandType.SUBCOMMAND,
-        "set",
-        "Set a nickname",
-        null,
-        false,
-        false,
-        null,
-        Arrays.asList(userOption, nickOption)
-      );
+      ApplicationCommandType.SUBCOMMAND,
+      "set",
+      "Set a nickname",
+      null,
+      false,
+      false,
+      null,
+      Arrays.asList(userOption, nickOption)
+    );
 
     var clearOption = new ApplicationCommandOption(
-        ApplicationCommandType.SUBCOMMAND,
-        "clear",
-        "Clear a nickname",
-        null,
-        false,
-        false,
-        null,
-        Arrays.asList(userOption)
-      );
+      ApplicationCommandType.SUBCOMMAND,
+      "clear",
+      "Clear a nickname",
+      null,
+      false,
+      false,
+      null,
+      Arrays.asList(userOption)
+    );
 
     commands.registerCommand(
       "nick",
       "Modify a nickname for a particular user",
       Arrays.asList(setOption, clearOption),
       args -> {
-        if (args.containsKey("set")){
+        if (args.containsKey("set")) {
           var id = (String) args.get("user");
           var nick = (String) args.get("nickname");
           sets.setString(String.valueOf(id), nick);
-          return new CommandsAPI.CommandResult("Set nickname successfuly", null, false);
+          return new CommandsAPI.CommandResult(
+            "Set nickname successfuly",
+            null,
+            false
+          );
         }
-        if (args.containsKey("clear")){
+        if (args.containsKey("clear")) {
           var id = (String) args.get("user");
           sets.setString(String.valueOf(id), null);
-          return new CommandsAPI.CommandResult("Cleared nickname successfuly", null, false);
+          return new CommandsAPI.CommandResult(
+            "Cleared nickname successfuly",
+            null,
+            false
+          );
         }
         return new CommandsAPI.CommandResult();
       }
-    );
-
-    patcher.patch(
-      GuildMember.Companion.getClass(),
-      "getNickOrUsername",
-      new Class<?>[] {
-        User.class,
-        GuildMember.class,
-        Channel.class,
-        List.class,
-      },
-      new PinePatchFn(
-        callFrame -> {
-          var user = (User) callFrame.args[0];
-          var userId = user.getId();
-          var nickname = sets.getString(String.valueOf(userId), null);
-          Utils.log(nickname);
-          if (nickname != null) {
-            callFrame.setResult(nickname);
-          }
-        }
-      )
     );
   }
 
