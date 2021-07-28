c9bb49a6c63a9bc5f508662af69530f3a132f060
wingio <mcprowing@gmail.com>
2021-07-27 22:02:56 -0400

    test

---
+++ HideCallButton/src/main/java/com/aliucord/plugins/HideCallButton.java
@@ -0,0 +1,81 @@
+package com.aliucord.plugins;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
+
+import com.aliucord.Logger;
+import com.aliucord.Utils;
+import com.aliucord.entities.Plugin;
+import com.aliucord.patcher.PinePatchFn;
+import com.aliucord.patcher.PinePrePatchFn;
+import com.discord.api.message.attachment.MessageAttachment;
+import com.discord.databinding.WidgetGuildProfileSheetBinding;
+import com.discord.utilities.SnowflakeUtils;
+import com.discord.utilities.icon.IconUtils;
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemAttachment;
+import com.discord.widgets.guilds.profile.WidgetGuildProfileSheet;
+import com.discord.widgets.guilds.profile.WidgetGuildProfileSheetViewModel;
+import com.discord.widgets.media.WidgetMedia;
+import com.discord.widgets.user.profile.UserProfileHeaderView;
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel;
+
+import java.lang.reflect.Field;
+
+
+@SuppressWarnings("unused")
+public class HideCallButton extends Plugin {
+    private static final Logger logger = new Logger("ViewProfileImages");
+    private static Field fileNameField;
+    private static Field idField;
+    private static Field urlField;
+    private static Field proxyUrlField;
+
+    @NonNull
+    @Override
+    public Manifest getManifest() {
+        var manifest = new Manifest();
+        manifest.authors =
+        new Manifest.Author[] {
+          new Manifest.Author("Wing", 298295889720770563L),
+        };
+        manifest.description = "Hides the video and call buttons";
+        manifest.version = "1.0.0";
+        manifest.updateUrl = "https://raw.githubusercontent.com/wingio/plugins/builds/updater.json";
+        return manifest;
+    }
+
+    @SuppressWarnings({"AccessStaticViaInstance", "JavaReflectionMemberAccess"})
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void start(Context context) throws Throwable {
+
+        final var getBindingMethod = WidgetGuildProfileSheet.class.getDeclaredMethod("getBinding");
+        getBindingMethod.setAccessible(true);
+
+        final int videoId = Utils.getResId("user_sheet_video_action_button", "id");
+        final int callId = Utils.getResId("user_sheet_call_action_button", "id");
+
+        patcher.patch(UserProfileHeaderView.class.getDeclaredMethod("configureBanner", UserProfileHeaderViewModel.ViewState.Loaded.class), new PinePatchFn(callFrame -> {
+            var binding = UserProfileHeaderView.access$getBinding$p((UserProfileHeaderView) callFrame.thisObject);
+            var root = binding.getRoot();
+
+            var videoView = root.findViewById(videoId);
+            var callView = root.findViewById(callId);
+
+            videoView.setVisibility(View.GONE);
+            videoView.setLayoutParams(new ViewGroup.LayoutParams(0, 0));
+            callView.setVisibility(View.GONE);
+            callView.setLayoutParams(new ViewGroup.LayoutParams(0, 0));
+        }));
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+    }
+}
