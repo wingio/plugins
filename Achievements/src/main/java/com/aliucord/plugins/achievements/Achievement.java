6aff920947b7297baa50d55a1904dc703620717c
wingio <mcprowing@gmail.com>
2021-07-28 18:06:48 -0400

    test

--- Achievements/src/main/java/com/aliucord/plugins/achievements/Achievement.java
+++ Achievements/src/main/java/com/aliucord/plugins/achievements/Achievement.java
@@ -27,32 +27,33 @@ class Achievement {
     private static String name;
     private static String description;
     private static String id;
+    private static SettingsAPI sets;
 
     public Achievement(Context ctx, String name, String description, String id) {
         this.name = name;
         this.description = description;
         this.id = id;
-        this.settings = PluginManager.plugins.get("Achievements").settings;
+        this.sets = PluginManager.plugins.get("Achievements").settings;
     }
 
     public static boolean isUnlocked() {
-        return this.settings.getBool("ach_" + this.id, false);
+        return sets.getBool("ach_" + id, false);
     }
 
     public static void unlock() {
         if(this.isUnlocked()){
             NotificationData notD = new NotificationData();
             notD.setTitle("Achievement Unlocked");
-            notD.setBody(Utils.renderMD("**" + this.name + "**: " + this.description));
+            notD.setBody(Utils.renderMD("**" + name + "**: " + description));
             notD.setAutoDismissPeriodSecs(5);
             notD.setIconUrl("https://media.discordapp.net/attachments/656712865344126997/869987570090655784/76018874.png");
             notD.setOnClick(v -> {
-                Utils.log("Achievement Unlocked: ach_" + this.id);
+                Utils.log("Achievement Unlocked: ach_" + id);
                 return Unit.a;
             });
     
             NotificationsAPI.display(notD);
-            this.settings.setBool("ach_" + this.id, true);
+            sets.setBool("ach_" + id, true);
         }
     }
 }
