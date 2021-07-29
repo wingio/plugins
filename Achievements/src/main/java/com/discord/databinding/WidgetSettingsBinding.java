0f9098348a3e4cade8b49c97c81d90da1e9eb7af
wingio <mcprowing@gmail.com>
2021-07-29 19:49:30 -0400

    test

--- Achievements/src/main/java/com/discord/databinding/WidgetSettingsBinding.java
+++
@@ -1,8 +0,0 @@
-package com.discord.databinding;
-
-import android.view.View;
-
-@SuppressWarnings("unused")
-public final class WidgetSettingsBinding {
-    public View getRoot() { return getRoot(); };
-}
