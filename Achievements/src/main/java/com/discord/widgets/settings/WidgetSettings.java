d9d58f75011b63308f1414194c59d9005eda726b
wingio <mcprowing@gmail.com>
2021-07-29 19:31:06 -0400

    test

--- Achievements/src/main/java/com/discord/widgets/settings/WidgetSettings.java
+++ Achievements/src/main/java/com/discord/widgets/settings/WidgetSettings.java
@@ -1,6 +1,7 @@
 package com.discord.widgets.settings;
 
 import com.discord.app.AppFragment;
+import android.view.View;
 
 public class WidgetSettings extends AppFragment {
     public static final class Model { }
