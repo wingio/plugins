74882b803214b429addfd430a68f6bb5ca5f9e27
wingio <mcprowing@gmail.com>
2021-07-29 20:03:02 -0400

    test

--- Achievements/src/main/java/com/discord/widgets/settings/WidgetSettings.java
+++ Achievements/src/main/java/com/discord/widgets/settings/WidgetSettings.java
@@ -1,7 +1,12 @@
 package com.discord.widgets.settings;
 
 import com.discord.app.AppFragment;
+import com.discord.databinding.WidgetSettingsBinding;
 
 public class WidgetSettings extends AppFragment {
     public static final class Model { }
+
+    private WidgetSettingsBinding getBinding() { return new WidgetSettingsBinding(); }
+
+    public configureUI(Model model) {}
 }
