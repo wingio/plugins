d2f28b9211cfaf1beba549d20a35e097fd90efe9
Wing <44992537+wingio@users.noreply.github.com>
2021-08-05 01:55:46 -0400

    Update TestPlugin.java

--- TestPlugin/src/main/java/com/aliucord/plugins/TestPlugin.java
+++ TestPlugin/src/main/java/com/aliucord/plugins/TestPlugin.java
@@ -72,7 +72,9 @@ public class TestPlugin extends Plugin {
                 Utils.log("Last Changed: " + ctx.getString(2131887250));
                 Utils.log("Revision: " + ctx.getString(2131887252));
                 Utils.log("Video: " + ctx.getString(2131887253));
+                /*WidgetChangeLog.Companion.launch(ctx, ctx.getString(2131887250), ctx.getString(2131887252), ctx.getString(2131887253), body);*/
             });
+           
             view.addView(option, 4);
         }));
     }
