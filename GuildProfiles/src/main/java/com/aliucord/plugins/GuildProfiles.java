8251fa9b41723b5061881ccae24c87a8f9a54fd2
wingio <mcprowing@gmail.com>
2021-08-17 14:50:45 -0400

    test

--- GuildProfiles/src/main/java/com/aliucord/plugins/GuildProfiles.java
+++ GuildProfiles/src/main/java/com/aliucord/plugins/GuildProfiles.java
@@ -124,7 +124,7 @@ public class GuildProfiles extends Plugin {
                     discrim = "0" + discrim;
                   }
                   addInfo(ctx, info, "Owner", owner.getUsername() + "#" + discrim, e -> {
-                      var fm = new SettingsPage().getParentFragmentManager();
+                      var fm = _this.getParentFragmentManager();
                      
                       WidgetUserSheet.Companion.show(owner.getId(), fm);
                       return true;
