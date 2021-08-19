6665cc11b47e9911b0f528b9b86c625f392e2054
wingio <mcprowing@gmail.com>
2021-08-18 21:49:49 -0400

    test

--- FavoriteMessages/src/main/java/com/aliucord/plugins/favoritemessages/MessageCard.java
+++ FavoriteMessages/src/main/java/com/aliucord/plugins/favoritemessages/MessageCard.java
@@ -78,7 +78,6 @@ public class MessageCard extends MaterialCardView {
         tagView.setTextColor(Color.WHITE);
         tagView.setPadding(Utils.dpToPx(2), Utils.dpToPx(2), Utils.dpToPx(2), Utils.dpToPx(2));
         tagView.setBackgroundDrawable(ContextCompat.getDrawable(ctx, R.d.drawable_button_brand_neutral));
-        tagView.setVisibility(View.GONE);
 
         LinearLayout authorField = new LinearLayout(ctx);
         authorField.setOrientation(LinearLayout.HORIZONTAL);
