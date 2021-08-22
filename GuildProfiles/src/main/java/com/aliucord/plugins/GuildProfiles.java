ceb6e4b7e22b94483b29fd92eb62917425838538
Wing <44992537+wingio@users.noreply.github.com>
2021-08-22 12:29:13 -0400

    Update GuildProfiles.java

--- GuildProfiles/src/main/java/com/aliucord/plugins/GuildProfiles.java
+++ GuildProfiles/src/main/java/com/aliucord/plugins/GuildProfiles.java
@@ -95,15 +95,15 @@ public class GuildProfiles extends Plugin {
               Context ctx = layout.getContext();
               var clock = ClockFactory.get();
 
-              LinearLayout tabs = (LinearLayout) lo.findViewById(Utils.getResId("guild_profile_sheet_tab_items", "id"));
-              Button mutualBtn = new Button(tabs.getContext(), null, 0, Utils.getResId("GuildProfileSheet.TabItems.Button", "style"));
+              LinearLayout actions = (LinearLayout) ((FrameLayout) lo.findViewById(Utils.getResId("guild_profile_sheet_secondary_actions", "id"))).getChildAt(0);
+              TextView mutualBtn = new TextView(actions.getContext(), null, 0, Utils.getResId("GuildProfileSheet.Actions.Title", "style"));
               mutualBtn.setId(tabId);
-              mutualBtn.setText("Friends");
-              mutualBtn.setCompoundDrawablesWithIntrinsicBounds(0, R.d.ic_tab_friends, 0, 0);
+              mutualBtn.setText("Mutual Friends");
+              //mutualBtn.setCompoundDrawablesWithIntrinsicBounds(0, R.d.ic_tab_friends, 0, 0);
               mutualBtn.setTypeface(ResourcesCompat.getFont(tabs.getContext(), Constants.Fonts.whitney_semibold));
-              mutualBtn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
-              if(tabs.findViewById(tabId) == null) {
-                  tabs.addView(mutualBtn);
+              mutualBtn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+              if(actions.findViewById(tabId) == null) {
+                  actions.addView(mutualBtn, 2);
               }
 
               GridLayout info = new GridLayout(ctx);
