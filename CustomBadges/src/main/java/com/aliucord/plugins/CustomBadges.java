e8aec6007f6acc9efc6e3d4b7eb19a64af200327
wingio <mcprowing@gmail.com>
2021-09-08 20:29:30 -0400

    Release CustomBadges

--- CustomBadges/src/main/java/com/aliucord/plugins/CustomBadges.java
+++ CustomBadges/src/main/java/com/aliucord/plugins/CustomBadges.java
@@ -14,6 +14,7 @@ import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.Constants;
 import com.aliucord.Utils;
 import com.aliucord.Logger;
