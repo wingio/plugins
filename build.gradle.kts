619b37877f05884b8eda74d9cd7412362ebbdf26
wingio <mcprowing@gmail.com>
2021-10-08 20:38:22 -0400

    Switch to main branch in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ subprojects {
         val implementation by configurations
 
         discord("com.discord:discord:96102")
-        implementation("com.github.Aliucord:Aliucord:dev-SNAPSHOT")
+        implementation("com.github.Aliucord:Aliucord:main-SNAPSHOT")
 
         implementation("androidx.appcompat:appcompat:1.3.1")
         implementation("com.google.android.material:material:1.4.0")
