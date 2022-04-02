29ce597d31868bced41d09f2bee8c8cc1ae4d01f
autodistries <automatedactions@protonmail.com>
2022-04-02 21:15:06 +0200

    added a changelog because it's cooler

--- MessageLinkContext/build.gradle.kts
+++ MessageLinkContext/build.gradle.kts
@@ -1,3 +1,11 @@
 version = "1.7.0"
 description = "Adds a context menu option to copy the message link."
-aliucord.author("ℵ✺℘ḙ", 532967505438965780L)
+aliucord {
+    author("ℵ✺℘ḙ", 532967505438965780L)
+    changelog.set("""
+        Added a settings page that allows you to toggle:
+        - Replace share option
+        - Remove share option
+        - Add copy url option
+    """.trimIndent())
+}
