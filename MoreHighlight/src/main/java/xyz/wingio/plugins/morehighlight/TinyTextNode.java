679283453c5d95ba1525e51a074f00d6a822a2fe
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-21 13:16:30 -0400

    Create TinyTextNode.java

---
+++ MoreHighlight/src/main/java/xyz/wingio/plugins/morehighlight/TinyTextNode.java
@@ -0,0 +1,33 @@
+package xyz.wingio.plugins.morehighlight;
+
+import com.discord.simpleast.core.node.Node;
+import com.discord.utilities.textprocessing.*;
+import com.discord.utilities.color.ColorCompat;
+
+import android.text.SpannableStringBuilder;
+import android.text.style.*;
+import android.graphics.Color;
+
+import com.lytefast.flexinput.R;
+
+public class TinyTextNode<MessageRenderContext> extends Node<MessageRenderContext> {
+  String content;
+
+  public TinyTextNode(String content){
+    super();
+    this.content = content;
+  }
+
+  @Override
+  public void render(SpannableStringBuilder builder, MessageRenderContext renderContext) {
+    int length = builder.length();
+    builder.append(content);
+    
+    // Make text smaller
+    builder.setSpan(new RelativeSizeSpan(0.85f), length, builder.length(), SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+    
+    // Make text grey
+    int greyColor = ColorCompat.getThemedColor(renderContext.getContext(), R.b.colorTextMuted);
+    builder.setSpan(new ForegroundColorSpan(greyColor), length, builder.length(), SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+  }
+}
