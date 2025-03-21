1d27b088c75e0588e69ed752f7f59bd14893a054
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-21 16:17:46 -0400

    changelog

--- MoreHighlight/build.gradle.kts
+++ MoreHighlight/build.gradle.kts
@@ -1,10 +1,12 @@
-version = "1.3.0"
+version = "1.4.0"
 description = "Adds more syntax highlighting options"
 
 aliucord.changelog.set(
     """
     New {added marginTop}
     ======================
+    * Added headers and subtext -serinova
+
 
     * Backported slash command highlighting
     """.trimIndent()
