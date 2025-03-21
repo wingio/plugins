b7e0d112510634329c558850a625acb6049f0186
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-21 16:33:30 -0400

    me dumb

--- MoreHighlight/src/main/java/xyz/wingio/plugins/morehighlight/SubtextRule.java
+++ MoreHighlight/src/main/java/xyz/wingio/plugins/morehighlight/SubtextRule.java
@@ -7,19 +7,19 @@ import com.discord.utilities.textprocessing.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class TinyTextRule extends Rule<MessageRenderContext, TinyTextNode<MessageRenderContext>, MessageParseState> {
+public final class SubtextRule extends Rule<MessageRenderContext, SubtextNode<MessageRenderContext>, MessageParseState> {
 
-    public TinyTextRule() {
+    public SubtextRule() {
         // Matches tiny text syntax: -# tiny greyed out text
         super(Pattern.compile("^-# (.+)$"));
     }
 
     @Override
-    public ParseSpec<MessageRenderContext, MessageParseState> parse(Matcher matcher, Parser<MessageRenderContext, ? super TinyTextNode<MessageRenderContext>, MessageParseState> parser, MessageParseState s) {
+    public ParseSpec<MessageRenderContext, MessageParseState> parse(Matcher matcher, Parser<MessageRenderContext, ? super SubtextNode<MessageRenderContext>, MessageParseState> parser, MessageParseState s) {
         // Extract text content
         String content = matcher.group(1);
         
-        TinyTextNode tinyTextNode = new TinyTextNode(content);
-        return new ParseSpec<>(tinyTextNode, s);
+        SubtextNode SubtextNode = new SubtextNode(content);
+        return new ParseSpec<>(SubtextNode, s);
     }
 }
